1995193ffe1afa764f8820bab4b20882a4780575
SKYooon <ucs0191@naver.com>
2022-04-06 12:20:42 +0900

    Touch down event에 대해 비행기 위치 설정

--- Projects/SampleGame/app/src/main/java/com/example/samplegame/GameView.java
+++ Projects/SampleGame/app/src/main/java/com/example/samplegame/GameView.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Choreographer;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -89,4 +90,14 @@ public class GameView extends View implements Choreographer.FrameCallback {
         //Log.d(TAG, "onDraw()");
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            fighter.setPosition(x, y);
+        }
+        return super.onTouchEvent(event);
+    }
 }
