6fa7d31dcfd21d4643f527642a7699102dff87b6
SKYooon <ucs0191@naver.com>
2022-04-06 12:12:58 +0900

    Fighter class 생성

--- Projects/SampleGame/app/src/main/java/com/example/samplegame/GameView.java
+++ Projects/SampleGame/app/src/main/java/com/example/samplegame/GameView.java
@@ -23,6 +23,7 @@ public class GameView extends View implements Choreographer.FrameCallback {
     private static final int BALL_COUNT = 10;
 //    Ball ball1, ball2;
     private ArrayList<Ball> balls = new ArrayList<>();
+    private Fighter fighter;
     private long previousTimeNanos;
     private int framePerSecond;
     private Paint fpsPaint = new Paint();
@@ -37,14 +38,11 @@ public class GameView extends View implements Choreographer.FrameCallback {
     private void InitView() {
         view = this;
 
-        Resources res = getResources();
-        Bitmap soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
-        Ball.setBitmap(soccerBitmap);
+//        Resources res = getResources();
+//        Bitmap soccerBitmap = BitmapFactory.decodeResource(res, R.mipmap.soccer_ball_240);
+//        Ball.setBitmap(soccerBitmap);
 
-//        Ball ball1 = new Ball(10, 10);
-//        Ball ball2 = new Ball(7, 15);
-//        balls.add(ball1);
-//        balls.add(ball2);
+        fighter = new Fighter();
         Random random = new Random();
         for (int i =0; i < BALL_COUNT; i++) {
             int dx = random.nextInt(10) + 5;
@@ -64,29 +62,29 @@ public class GameView extends View implements Choreographer.FrameCallback {
         long now = currentTimeNanos;
         //long now = System.currentTimeMillis();
         int elapsed = (int) (now - previousTimeNanos);
-        framePerSecond = 1_000_000_000 / elapsed;
-        Log.v(TAG, "Elapsed: " + elapsed + " FPS: " + framePerSecond);
-        previousTimeNanos = now;
-        update();
-        invalidate();
+        if (elapsed != 0) {
+            framePerSecond = 1_000_000_000 / elapsed;
+            Log.v(TAG, "Elapsed: " + elapsed + " FPS: " + framePerSecond);
+            previousTimeNanos = now;
+            update();
+            invalidate();
+        }
         Choreographer.getInstance().postFrameCallback(this);
     }
 
     private void update() {
-//        ball1.update();
-//        ball2.update();
         for (Ball ball : balls) {
             ball.update();
         }
+//        fighter.update();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-//        ball1.draw(canvas);
-//        ball2.draw(canvas);
         for (Ball ball : balls) {
             ball.draw(canvas);
         }
+        fighter.draw(canvas);
         canvas.drawText("FPS: " + framePerSecond, framePerSecond * 10,100, fpsPaint);
         //Log.d(TAG, "onDraw()");
     }
